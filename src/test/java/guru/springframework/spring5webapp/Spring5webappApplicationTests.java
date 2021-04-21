c7526a665322d30d82ee75979e9eafa7861ee230
Mikhail Bykov <mbykov@bullfrog.name.net>
2021-04-21 03:37:55 -0400

    First intermediate commit.

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp;
 
+/*
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,3 +15,4 @@ public class Spring5webappApplicationTests {
 	}
 
 }
+*/
